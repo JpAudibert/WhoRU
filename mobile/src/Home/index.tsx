0385709e0fcb92a41f30d531fbddbf78116e2f03
João Pedro Basso Audibert <hotsjoao@gmail.com>
2023-10-17 21:19:40 -0300

    Project simplification

--- mobile/src/Home/index.tsx
+++ mobile/src/Home/index.tsx
@@ -1,41 +1,49 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import { Camera, CameraType, FaceDetectionResult } from 'expo-camera';
-import * as FaceDetector from 'expo-face-detector';
-import { useEffect, useState, useRef } from 'react';
-import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { Camera, CameraType, type FaceDetectionResult } from 'expo-camera'
+import * as FaceDetector from 'expo-face-detector'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated'
 import { captureRef } from 'react-native-view-shot'
 
-import { styles } from './styles';
-import api from '../../services/api';
-import React from 'react';
+import { styles } from './styles'
+import api from '../../services/api'
 
 interface IRecognitionResult {
-  name: string;
-  match_percentage: number;
-  match_status: string;
+  name: string
+  match_percentage: number
+  match_status: string
 }
 
 const initialRecognitionResult: IRecognitionResult = {
-  name: "",
+  name: '',
   match_percentage: 0,
-  match_status: "false",
-};
+  match_status: 'false',
+}
+const initialDetectionInterval = 2000
 
 export function Home() {
-  let cameraRef = useRef<Camera>();
+  const cameraRef = useRef<Camera>()
 
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [permission, requestPermission] = Camera.useCameraPermissions()
 
-  const [faceDetected, setFaceDetected] = useState(false);
-  const [identity, setIdentity] = useState<IRecognitionResult>({} as IRecognitionResult);
-  const [snapshotImg, setSnapshotImg] = useState<string>();
+  const [faceDetected, setFaceDetected] = useState(false)
+  const [identity, setIdentity] = useState<IRecognitionResult>(
+    initialRecognitionResult,
+  )
+
+  const [detectionInterval, setDetectionInterval] = useState(
+    initialDetectionInterval,
+  )
 
   const faceValues = useSharedValue({
     width: 0,
     height: 0,
     x: 0,
-    y: 0
-  });
+    y: 0,
+  })
 
   const animatedStyle = useAnimatedStyle(() => ({
     position: 'absolute',
@@ -48,95 +56,106 @@ export function Home() {
     ],
     borderColor: 'blue',
     borderWidth: 10,
-  }));
+  }))
 
-  async function takePic() {
+  const takePic = useCallback(async (): Promise<string> => {
     try {
-      let capturedSnapshot = await captureRef(cameraRef, {
+      return await captureRef(cameraRef, {
         format: 'png',
         quality: 0.8,
-        result: "base64",
+        result: 'base64',
         width: 720,
         height: 720,
-      });
-
-      setSnapshotImg(capturedSnapshot);
+      })
     } catch (ex) {
-      console.error(ex, "Some error have occurred capturing the face image");
+      console.error(ex, 'Some error have occurred capturing the face image')
     }
-  };
+  }, [])
 
-  async function handleFacesDetected({ faces }: FaceDetectionResult) {
-    const face = faces[0] as FaceDetector.FaceFeature;
-    setFaceDetected(false);
+  const handleFacesDetected = useCallback(
+    async ({ faces }: FaceDetectionResult) => {
+      const face = faces[0] as FaceDetector.FaceFeature
+      setFaceDetected(false)
 
-    console.log("not identifying")
-    if (face && !identity.name) {
-      console.log("identifying");
+      console.log('not identifying')
+      if (face && !identity.name) {
+        console.log('identifying')
 
-      const { size, origin } = face.bounds;
+        const { size, origin } = face.bounds
 
-      faceValues.value = {
-        width: size.width,
-        height: size.height,
-        x: origin.x,
-        y: origin.y,
+        faceValues.value = {
+          width: size.width,
+          height: size.height,
+          x: origin.x,
+          y: origin.y,
+        }
+
+        const capturedSnapshot = await takePic()
+
+        console.log('image base64', capturedSnapshot.slice(0, 100))
+
+        try {
+          const { data } = await api.post<IRecognitionResult>(
+            'identify',
+            {
+              data: capturedSnapshot,
+            },
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            },
+          )
+
+          console.log('request data', data)
+          setIdentity(data)
+          setDetectionInterval(20000)
+        } catch (error) {
+          console.log(error)
+        }
+
+        setFaceDetected(true)
       }
+    },
+    [faceValues, identity.name, takePic],
+  )
 
-      takePic();
+  const handleConfirmation = useCallback(
+    async (confirmation: boolean) => {
+      console.log(confirmation)
 
-      try {
-        let { data } = await api.post<IRecognitionResult>("identify", {
-          data: snapshotImg
-        }, {
+      const confirmationString = confirmation + ''
+
+      await api.post(
+        'confirmation',
+        {
+          name: identity.name,
+          confirmation: confirmationString,
+        },
+        {
           headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        });
+            'Content-Type': 'multipart/form-data',
+          },
+        },
+      )
 
-        console.log(data);
-        setIdentity(data);
-      } catch (error) {
-        console.log(error)
-      }
-
-      setFaceDetected(true);
-      setSnapshotImg("");
-    }
-  }
-
-  async function handleConfirmation(confirmation: boolean) {
-    console.log(confirmation);
-
-    let confirmationString = confirmation + ""
-
-    console.log(confirmationString);
-
-    await api.post("confirmation", {
-      name: identity.name,
-      confirmation: confirmationString
-    }, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    });
-
-    setIdentity(initialRecognitionResult);
-  }
+      setIdentity(initialRecognitionResult)
+      setDetectionInterval(initialDetectionInterval)
+    },
+    [identity.name],
+  )
 
   useEffect(() => {
-    requestPermission();
-  }, []);
+    requestPermission()
+  }, [requestPermission])
 
   if (!permission?.granted) {
-    return;
+    return
   }
 
   return (
     <View style={styles.container}>
-      {
-        faceDetected && <Animated.View style={animatedStyle} />
-      }
+      {faceDetected && <Animated.View style={animatedStyle} />}
       <Camera
         ref={cameraRef}
         style={styles.camera}
@@ -146,27 +165,38 @@ export function Home() {
           mode: FaceDetector.FaceDetectorMode.accurate,
           detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
           runClassifications: FaceDetector.FaceDetectorClassifications.all,
-          minDetectionInterval: 2000,
+          minDetectionInterval: detectionInterval,
           tracking: false,
         }}
       />
 
-      {
-        !!identity.name &&
+      {!!identity.name && (
         <View style={styles.confirmation}>
           <View style={styles.confirmationContainerText}>
-            <Text style={styles.confirmationText}>You are {identity.name}. Is it correct</Text>
+            <Text style={styles.confirmationText}>
+              You are {identity.name}. Is it correct?
+            </Text>
           </View>
           <View style={styles.confirmationBox}>
-            <TouchableOpacity style={styles.confirmationButton} onPress={() => handleConfirmation(true)}>
+            <TouchableOpacity
+              style={styles.confirmationButton}
+              onPress={async () => {
+                await handleConfirmation(true)
+              }}
+            >
               <Text style={styles.confirmationButtonText}>Yes!</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.confirmationButtonNegation} onPress={() => handleConfirmation(false)}>
+            <TouchableOpacity
+              style={styles.confirmationButtonNegation}
+              onPress={async () => {
+                await handleConfirmation(false)
+              }}
+            >
               <Text style={styles.confirmationButtonText}>No.</Text>
             </TouchableOpacity>
           </View>
         </View>
-      }
+      )}
     </View>
-  );
+  )
 }
