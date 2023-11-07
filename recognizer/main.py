f3dcd2ffd8c6be646c80bebad578242e0df1264d
João Pedro Basso Audibert <hotsjoao@gmail.com>
2023-11-07 15:08:11 -0300

    Removes reload

--- recognizer/main.py
+++ recognizer/main.py
@@ -269,4 +269,4 @@ async def get_attendance_logs():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=False)
